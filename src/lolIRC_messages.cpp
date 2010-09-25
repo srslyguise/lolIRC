535eb1bf3539efa1dcfe0c6648c5081f2b653901
edx <edx1010@gmail.com>
2010-09-25 18:04:49 +0200

    Written Nick and User messages

--- src/lolIRC_messages.cpp
+++ src/lolIRC_messages.cpp
@@ -1,6 +1,13 @@
+/***************************************************************************
+ * Copyleft EDX. http://edx.shellmix.com, edx1010@gmail.com
+ *
+ * You like this software? Do what you want under the terms of
+ * GNU General Public License.
+ ***************************************************************************/
+
 #include <lolIRC.hpp>
 
-void lolIRC::Client::lolIRC_Client::join(std::string chan_name, std::string key)
+void lolIRC::Client::lolIRC_Client::Join(std::string chan_name, std::string key)
 {
 	std::string message = "JOIN ";
 
@@ -16,11 +23,36 @@ void lolIRC::Client::lolIRC_Client::join(std::string chan_name, std::string key)
 	connection.Write(message);
 }
 
-void lolIRC::Client::lolIRC_Client::part(std::string chan_name)
+void lolIRC::Client::lolIRC_Client::Part(std::string chan_name)
 {
 	std::string message = "PART ";
 
 	message += chan_name;
 	message += "\r\n";
+	connection.Write(message);
+}
+
+void lolIRC::Client::lolIRC_Client::Nick(std::string n)
+{
+	std::string message = "NICK ";
+
+	message += n;
+	message += "\r\n";
+	connection.Write(message);
+}
+
+void lolIRC::Client::lolIRC_Client::User(std::string u, std::string rn, std::string mode)
+{
+	std::string message = "USER ";
+
+	message += u;
+	message += ' ';
+	message += mode;
+	message += ' ';
+	message += u;
+	message += " :";
+	message += rn;
+	message += "\r\n";
+
 	connection.Write(message);
 }
